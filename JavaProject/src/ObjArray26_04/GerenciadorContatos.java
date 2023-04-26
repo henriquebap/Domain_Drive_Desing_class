8494f1e4dd6950644cf342933af92494d20027f8
henriquebap <henriquebaptista2003@gmail.com>
2023-04-26 09:45:01 -0300

    Crinado o objeto de mostrar contato usando method get

--- JavaProject/src/ObjArray26_04/GerenciadorContatos.java
+++ JavaProject/src/ObjArray26_04/GerenciadorContatos.java
@@ -2,8 +2,6 @@ package ObjArray2_04;
 
 import java.util.Scanner;
 
-import javax.swing.text.AbstractDocument.Content;
-
 public class GerenciadorContatos{
 
     private Contato[] agenda;
@@ -11,7 +9,7 @@ public class GerenciadorContatos{
         agenda = new Contato[tamanho];
     }
 
-    public void criarAgenda(){
+    public Contato[] criarAgenda(){
         Scanner input = new Scanner(System.in);
         
         for(int i=0; i < agenda.length; i++){
@@ -19,6 +17,18 @@ public class GerenciadorContatos{
             String nome = input.next();
             System.out.println("Telefone: ");
             String telefone = input.next();
+
+            //criando Contato
+            Contato c = new Contato(nome, telefone);
+            agenda[i] = c;
+        }
+        return agenda;
+    }
+    public void mostraConato(){
+        for(i=0; i<agenda.length; i++){
+            System.out.println("Nome: " + agenda[i].getName());
+            System.out.println("Telefone: " + agenda[i].getTelefone());
+            
         }
     }
 }
