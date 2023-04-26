3c8cd74b4c96be3669696a4456fde223b6012c72
henriquebap <henriquebaptista2003@gmail.com>
2023-04-26 09:24:35 -0300

    Criando o laço de repetição usando agenda.length

--- JavaProject/src/ObjArray26_04/GerenciadorContatos.java
+++ JavaProject/src/ObjArray26_04/GerenciadorContatos.java
@@ -1,5 +1,7 @@
 package ObjArray2_04;
 
+import java.util.Scanner;
+
 import javax.swing.text.AbstractDocument.Content;
 
 public class GerenciadorContatos{
@@ -9,7 +11,14 @@ public class GerenciadorContatos{
         agenda = new Contato[tamanho];
     }
 
-    public void addContato(Contato c){
+    public void criarAgenda(){
+        Scanner input = new Scanner(System.in);
         
+        for(int i=0; i < agenda.length; i++){
+            System.out.println("Nome: ");
+            String nome = input.next();
+            System.out.println("Telefone: ");
+            String telefone = input.next();
+        }
     }
 }
